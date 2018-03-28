a262f22ed4b8688bf9cb87cdaf9c6ceb19d8a6a5
Christian Terence <30540602+christerence@users.noreply.github.com>
2018-03-28 11:48:27 -0400

    Login UI/UX Design

--- app/src/main/java/deaddevs/com/studentcompanion/MainActivity.java
+++ app/src/main/java/deaddevs/com/studentcompanion/MainActivity.java
@@ -2,6 +2,7 @@ package deaddevs.com.studentcompanion;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 }
