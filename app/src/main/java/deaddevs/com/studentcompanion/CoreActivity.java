7e750a540eda774c4045bcd824d82e756505ef4c
Christian Terence <30540602+christerence@users.noreply.github.com>
2018-04-17 13:38:33 -0400

    Essential Navigations Done

--- app/src/main/java/deaddevs/com/studentcompanion/CoreActivity.java
+++ app/src/main/java/deaddevs/com/studentcompanion/CoreActivity.java
@@ -2,10 +2,18 @@ package deaddevs.com.studentcompanion;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
 
 public class CoreActivity extends AppCompatActivity {
     CourseListFragment course;
+    AccountFragment profile;
+    SettingsFragment settings;
+    CalendarFragment calendar;
+    CoursePageFragment coursepage;
+    AssignmentPageFragment assignmentpage;
+
+    String currPage = "Course";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +32,72 @@ public class CoreActivity extends AppCompatActivity {
             getSupportFragmentManager().beginTransaction().add(R.id.CourseList, course).commit();
         }
     }
+
+    public void handleNav(View v) {
+        getSupportFragmentManager().beginTransaction().remove(course).commit();
+        switch(v.getId()) {
+            case R.id.ProfilePic:
+                currPage = "Profile";
+                if(findViewById(R.id.Profile) != null) {
+                    profile = new AccountFragment();
+                    profile.setArguments(getIntent().getExtras());
+                    getSupportFragmentManager().beginTransaction().add(R.id.Profile, profile).commit();
+                }
+                break;
+            case R.id.SettingPic:
+                currPage = "Settings";
+                if(findViewById(R.id.Profile) != null) {
+                    settings = new SettingsFragment();
+                    settings.setArguments(getIntent().getExtras());
+                    getSupportFragmentManager().beginTransaction().add(R.id.Settings, settings).commit();
+                }
+                break;
+            case R.id.TodoPic:
+                currPage = "ToDo";
+                if(findViewById(R.id.Profile) != null) {
+                    calendar = new CalendarFragment();
+                    calendar.setArguments(getIntent().getExtras());
+                    getSupportFragmentManager().beginTransaction().add(R.id.ToDoList, calendar).commit();
+                }
+                break;
+        }
+    }
+
+
+    public void handleBack(View v) {
+        switch(currPage) {
+            case "Profile":
+                getSupportFragmentManager().beginTransaction().remove(profile).commit();
+                if(findViewById(R.id.CourseList) != null) {
+                    course = new CourseListFragment();
+                    course.setArguments(getIntent().getExtras());
+                    getSupportFragmentManager().beginTransaction().add(R.id.CourseList, course).commit();
+                }
+                currPage = "Course";
+                break;
+            case "Settings":
+                getSupportFragmentManager().beginTransaction().remove(settings).commit();
+                if(findViewById(R.id.CourseList) != null) {
+                    course = new CourseListFragment();
+                    course.setArguments(getIntent().getExtras());
+                    getSupportFragmentManager().beginTransaction().add(R.id.CourseList, course).commit();
+                }
+                currPage = "Course";
+                break;
+            case "ToDo":
+                getSupportFragmentManager().beginTransaction().remove(calendar).commit();
+                if(findViewById(R.id.CourseList) != null) {
+                    course = new CourseListFragment();
+                    course.setArguments(getIntent().getExtras());
+                    getSupportFragmentManager().beginTransaction().add(R.id.CourseList, course).commit();
+                }
+                currPage = "Course";
+                break;
+        }
+
+    }
+
+
 
 
 }
