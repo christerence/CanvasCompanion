91be1d888fc866cae302be059cc6e717a61cb623
Christian Terence <30540602+christerence@users.noreply.github.com>
2018-04-21 15:00:30 -0400

    TodoList Functionality Done

--- app/src/main/java/deaddevs/com/studentcompanion/AccountFragment.java
+++ app/src/main/java/deaddevs/com/studentcompanion/AccountFragment.java
@@ -13,12 +13,7 @@ import deaddevs.com.studentcompanion.utils.FontAwesomeHelper;
 
 
 public class AccountFragment extends Fragment {
-
-    //private OnFragmentInteractionListener mListener;
-
-    public AccountFragment() {
-        // Required empty public constructor
-    }
+    public AccountFragment() {}
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,43 +24,4 @@ public class AccountFragment extends Fragment {
         back.setTypeface(FontAwesomeHelper.getTypeface(getContext(), FontAwesomeHelper.FONTAWESOME));
         return v;
     }
-
-//    // TODO: Rename method, update argument and hook method into UI event
-//    public void onButtonPressed(Uri uri) {
-//        if (mListener != null) {
-//            mListener.onFragmentInteraction(uri);
-//        }
-//    }
-//
-//    @Override
-//    public void onAttach(Context context) {
-//        super.onAttach(context);
-//        if (context instanceof OnFragmentInteractionListener) {
-//            mListener = (OnFragmentInteractionListener) context;
-//        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-//        }
-//    }
-//
-//    @Override
-//    public void onDetach() {
-//        super.onDetach();
-//        mListener = null;
-//    }
-//
-//    /**
-//     * This interface must be implemented by activities that contain this
-//     * fragment to allow an interaction in this fragment to be communicated
-//     * to the activity and potentially other fragments contained in that
-//     * activity.
-//     * <p>
-//     * See the Android Training lesson <a href=
-//     * "http://developer.android.com/training/basics/fragments/communicating.html"
-//     * >Communicating with Other Fragments</a> for more information.
-//     */
-//    public interface OnFragmentInteractionListener {
-//        // TODO: Update argument type and name
-//        void onFragmentInteraction(Uri uri);
-//    }
 }
