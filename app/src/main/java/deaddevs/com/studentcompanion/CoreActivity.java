8effeb5e45737c9aa3fa21db819c0ef44e501f20
Christian Terence <30540602+christerence@users.noreply.github.com>
2018-04-28 20:31:31 -0400

    Clean Up

--- app/src/main/java/deaddevs/com/studentcompanion/CoreActivity.java
+++ app/src/main/java/deaddevs/com/studentcompanion/CoreActivity.java
@@ -1,36 +1,22 @@
 package deaddevs.com.studentcompanion;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Canvas;
 import android.net.Uri;
 import android.os.PersistableBundle;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import org.json.JSONArray;
@@ -227,8 +213,6 @@ public class CoreActivity extends AppCompatActivity {
                     }
                     break;
             }
-
-
         }
     }
 
@@ -236,7 +220,6 @@ public class CoreActivity extends AppCompatActivity {
         //need to implement music service
         startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.youtube.com/watch?v=ISNBfryBkSo")));
     }
-
 
     @Override
     protected void onStart() {
