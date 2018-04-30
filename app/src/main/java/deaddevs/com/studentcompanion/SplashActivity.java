91d8bf878e47d9f98cab6dbb79d20836abe44053
Christian Terence <30540602+christerence@users.noreply.github.com>
2018-04-30 10:33:59 -0400

    Minor Clean up and Merge

---
+++ app/src/main/java/deaddevs/com/studentcompanion/SplashActivity.java
@@ -0,0 +1,33 @@
+package deaddevs.com.studentcompanion;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import deaddevs.com.studentcompanion.MainActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Thread timer=new Thread()
+        {
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally
+                {
+                    Intent i=new Intent(SplashActivity.this,MainActivity.class);
+                    finish();
+                    startActivity(i);
+                }
+            }
+        };
+        timer.start();
+    }
+}
